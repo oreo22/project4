bd4c3a08c7a3d7051cb7e1ad707b244de9a6754d
oreo22 <oriana22@utexas.edu>
2015-10-24 00:16:09 -0500

    started error handling
    ya

--- project4/Main.java
+++ project4/Main.java
@@ -16,12 +16,17 @@ public static void main(String[] args) throws InvalidCritterException {
 			System.out.print("critters>");
 			String commandLine=game.nextLine();
 			commands = commandLine.split(" ");
-//---------Avaliable Commands----------------
+//---------Available Commands----------------
 			switch (commands[0]) {
             case "quit": break outerloop;
             case "show":  CritterWorld.displayWorld(); break;
-            case "step":  if(commands.length==1){ CritterWorld.runWorld(1);}
-            			  else if(commands.length==2){CritterWorld.runWorld(Integer.parseInt(commands[1]));}
+            case "step":  if(commands.length==1){ CritterWorld.runWorld(1);
+            			  }
+            			  else if(commands.length==2){
+            				  //try{
+            				  CritterWorld.runWorld(Integer.parseInt(commands[1]));
+            				  //}catch (!StringUtils.isNumeric(commands[1])){System.out.print("Incorrect Command "); }
+            			   }
             				break;//put a try catch to see if commands[1] is a number
             case "seed": System.out.print("Work in Progress for seed"); break;
             case "make": System.out.print("Work in Progress for make"); break;
