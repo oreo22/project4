9adef605decb9d855051c001e6843cb73c1ec6ed
DiegoGuerra1994 <dguerra95@utexas.edu>
2015-11-05 23:34:19 -0600

    Trying to make Canvas

--- project4/CritterGUI.java
+++ project4/CritterGUI.java
@@ -1,4 +1,4 @@
-	package project4;
+	package project4;   
 	import javafx.application.Application;
 	import javafx.event.ActionEvent;
 	import javafx.event.EventHandler;
@@ -17,22 +17,31 @@
 	import javafx.scene.text.FontWeight;
 	import javafx.scene.text.Text;
 	import javafx.stage.Stage;
+	import javafx.scene.shape.*;
 public class CritterGUI extends Application{
 
 	
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("Critter Simulation");
 			
-		GridPane screen = new GridPane(); //makes the user-interactive part
+		GridPane grid = new GridPane(); //makes the user-interactive part
 		        //smaller part of the scene
 		        //Other panes exist
-		screen.setAlignment(Pos.CENTER_LEFT);
-		screen.setHgap(Params.world_height); //sets the height
-		screen.setVgap(Params.world_width); //sets the width? 
-		screen.setPadding(new Insets(25, 25, 25, 25)); 
+		grid.setAlignment(Pos.CENTER_LEFT);
+		grid.setHgap(20);
+		grid.setVgap(20);
+		grid.setPrefSize(20, 20);
+		primaryStage.setWidth(Params.world_width*5);
+		primaryStage.setHeight(Params.world_height*5);
 		
-		primaryStage.setWidth(Params.world_width*2);
-		primaryStage.setHeight(Params.world_height*2);
+		grid.setGridLinesVisible(true);
+		//screen.getChildren().get(0).setStyle("-fx-background-color: cornsilk; -fx-alignment: center;");
+		Scene scene = new Scene(grid, 300, 275);
+		grid.add(new Circle(5), 0, 0);
+		grid.add(new Circle(5), 5, 0);
+		grid.add(new Circle(5), 15, 2);
+		
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
